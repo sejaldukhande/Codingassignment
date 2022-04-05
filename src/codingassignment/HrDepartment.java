1693396d7c90146b31b960de9b6861fcb806a8ec
sejal <sejal.dukhande@saaacapital.com>
2022-04-05 23:20:43 +0530

    Coding Assignment

---
+++ src/codingassignment/HrDepartment.java
@@ -0,0 +1,16 @@
+package codingassignment;
+
+public class HrDepartment extends SuperDepartment{
+	public String departmentName() {
+		return "Welcome to Hr Department" ;
+	}
+	public String getTodaysWork() {
+		return "Fill today’s worksheet and mark your attendance";
+	}
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+	public String doActivity() {
+		return "team Lunch";
+	}
+}
